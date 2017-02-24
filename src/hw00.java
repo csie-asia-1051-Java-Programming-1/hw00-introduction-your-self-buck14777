6e36ecdd95fe9379825f895326028767308b6ccc
buck14777 <105021048@live.asia.edu.tw>
2017-02-24 15:13:03 +0800

    hw00

---
+++ src/hw00.java
@@ -0,0 +1,13 @@
+/*
+* Topic: 第一次作業
+* Date: 2017/02/24
+* Author: 105021048 李昂霖
+*/
+public class hw00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("學號:105021048\n"+"姓名:李昂霖");
+	}
+
+}
